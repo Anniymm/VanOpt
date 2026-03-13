f15edfa6e9dde241933da3a3c43f468f16de2e23
Anniymm <animaisuradze1999@gmail.com>
2026-03-13 13:13:57 +0400

    add: Spring Boot application entry point

---
+++ src/main/java/ge/kursi/vanopt/VanOptApplication.java
@@ -0,0 +1,12 @@
+package ge.kursi.vanopt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanOptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VanOptApplication.class, args);
+    }
+}
